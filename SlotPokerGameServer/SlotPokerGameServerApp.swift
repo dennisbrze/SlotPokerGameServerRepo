e44786abe99a795880e37ad66d7d6eaa21db9202
Dennis Grochowski <dennis.grochowski@aldi-sued.com>
2024-12-16 11:22:35 +0100

    Initial Commit

---
+++ SlotPokerGameServer/SlotPokerGameServerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlotPokerGameServerApp.swift
+//  SlotPokerGameServer
+//
+//  Created by Dennis Grochowski on 16.12.24.
+//
+
+import SwiftUI
+
+@main
+struct SlotPokerGameServerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
